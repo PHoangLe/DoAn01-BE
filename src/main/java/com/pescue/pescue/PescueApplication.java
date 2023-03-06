23d7bf96a2b585f346377905883f4baf7d786d19
PHoangLe <lehoangphuc180502@gmail.com>
2023-03-06 18:55:20 +0700

    First commit

---
+++ src/main/java/com/pescue/pescue/PescueApplication.java
@@ -0,0 +1,13 @@
+package com.pescue.pescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PescueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pescue/pescue/PescueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pescue.pescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
