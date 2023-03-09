99985b6ae342e4c01f2cf37872e8f87dfe10f409
PHoangLe <lehoangphuc180502@gmail.com>
2023-03-09 11:53:21 +0700

    cors config for all origin

--- src/main/java/com/pescue/pescue/controller/UserController.java
+++ src/main/java/com/pescue/pescue/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/v1/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "**")
 public class UserController {
 
     private final UserService userService;
