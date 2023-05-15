cdf78febe71c77b15b4d2c0ea04519f67e7ccc19
PHoangLe <lehoangphuc180502@gmail.com>
2023-05-15 11:58:39 +0700

    add getAdoptionApplicationByUserIDAndAnimalID API

--- src/main/java/com/pescue/pescue/controller/AdoptionController.java
+++ src/main/java/com/pescue/pescue/controller/AdoptionController.java
@@ -4,8 +4,10 @@ import com.pescue.pescue.dto.AdoptionApplicationDTO;
 import com.pescue.pescue.dto.AdoptionApplicationRequestDTO;
 import com.pescue.pescue.dto.StringResponseDTO;
 import com.pescue.pescue.exception.*;
+import com.pescue.pescue.model.AdoptionApplication;
 import com.pescue.pescue.model.OnlineAdoptionApplication;
 import com.pescue.pescue.service.AdoptionService;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,7 +32,7 @@ public class AdoptionController {
         try{
             service.createAdoptionRequest(dto);
         }
-        catch (UserNotFoundException | ShelterNotFoundException | AnimalNotFoundException e){
+        catch (UserNotFoundException | ShelterNotFoundException | AnimalNotFoundException | ApplicationExistedException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StringResponseDTO(e.getMessage()));
         }
         catch (Exception e){
@@ -60,6 +62,23 @@ public class AdoptionController {
         }
 
         return ResponseEntity.ok(applicationByShelterID);
+    }
+
+    @GetMapping("/getAdoptionApplicationByUserIDAndAnimalID/{userID}/{animalID}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<Object> getAdoptionApplicationByUserIDAndAnimalID(@PathVariable String userID, @PathVariable String animalID) {
+        try {
+            AdoptionApplication application = service.findApplicationByUserIDAndAnimalID(userID, animalID);
+
+            if (application != null)
+                ResponseEntity.ok(application);
+        }
+        catch (Exception e){
+            log.trace(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StringResponseDTO("Đã có lỗi xảy ra với hệ thống vui lòng thử lại sau"));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StringResponseDTO("Chưa tồn tại yêu cầu nhận nuôi"));
     }
 
     @PostMapping("/confirmAdoptionRequest/{adoptionApplicationID}")
@@ -175,5 +194,21 @@ public class AdoptionController {
         return ResponseEntity.ok(StringResponseDTO.builder()
                 .message("Đã từ chối yêu cầu nhận nuôi của bé")
                 .build());
+    }
+    @GetMapping("/getOnlineAdoptionApplicationByUserIDAndAnimalID/{userID}/{animalID}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    public ResponseEntity<Object> getOnlineAdoptionApplicationByUserIDAndAnimalID(@PathVariable String userID, @PathVariable String animalID) {
+        try {
+            OnlineAdoptionApplication application = service.findOnlineApplicationByUserIDAndAnimalID(userID, animalID);
+
+            if (application != null)
+                ResponseEntity.ok(application);
+        }
+        catch (Exception e){
+            log.trace(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new StringResponseDTO("Đã có lỗi xảy ra với hệ thống vui lòng thử lại sau"));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StringResponseDTO("Chưa tồn tại yêu cầu nhận nuôi"));
     }
 }
--- src/main/java/com/pescue/pescue/repository/OnlineAdoptionApplicationRepository.java
+++ src/main/java/com/pescue/pescue/repository/OnlineAdoptionApplicationRepository.java
@@ -1,5 +1,6 @@
 package com.pescue.pescue.repository;
 
+import com.pescue.pescue.model.AdoptionApplication;
 import com.pescue.pescue.model.OnlineAdoptionApplication;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +10,6 @@ import java.util.Optional;
 
 public interface OnlineAdoptionApplicationRepository extends MongoRepository<OnlineAdoptionApplication, String> {
     Optional<OnlineAdoptionApplication> findByApplicationID(@Param("applicationID") String applicationID);
+    Optional<OnlineAdoptionApplication> findByUserIDAndAnimalID(@Param("userID") String userID, @Param("animalID") String animalID);
     List<OnlineAdoptionApplication> findAllByShelterID(@Param("shelterID") String shelterID);
 }
