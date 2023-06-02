4528133f0cee9415d4a248310ce0f85fade5451c
PHoangLe <lehoangphuc180502@gmail.com>
2023-06-02 12:01:41 +0700

    add api getChatMessageByChatroomID

--- src/main/java/com/pescue/pescue/service/ChatRoomService.java
+++ src/main/java/com/pescue/pescue/service/ChatRoomService.java
@@ -1,11 +1,12 @@
 package com.pescue.pescue.service;
 
+import com.pescue.pescue.model.ChatMessage;
 import com.pescue.pescue.model.ChatRoom;
 import com.pescue.pescue.model.User;
+import com.pescue.pescue.repository.ChatMessageRepository;
 import com.pescue.pescue.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,25 +16,26 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class ChatRoomService {
-    private final ChatRoomRepository repository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatMessageRepository chatMessageRepository;
     private final UserService userService;
     public Optional<String> getChatId(String senderId, String recipientId, boolean createIfNotExist){
         User sender = userService.findUserByID(senderId);
         User recipient = userService.findUserByID(recipientId);
 
-        return repository
+        return chatRoomRepository
                 .findByUser1AndUser2(senderId, recipientId)
                 .map(ChatRoom::getChatRoomID)
                 .or(() -> {
                     if(!createIfNotExist)
                         return Optional.empty();
 
-                    return repository.findByUser1AndUser2(recipientId, senderId)
+                    return chatRoomRepository.findByUser1AndUser2(recipientId, senderId)
                             .map(ChatRoom::getChatRoomID)
                             .or(() -> {
                                 ChatRoom newChatRoom = new ChatRoom(sender, recipient);
 
-                                repository.insert(newChatRoom);
+                                chatRoomRepository.insert(newChatRoom);
                                 String chatRoomID = newChatRoom.getChatRoomID();
 
                                 return Optional.of(chatRoomID);
@@ -42,6 +44,10 @@ public class ChatRoomService {
     }
 
     public List<ChatRoom> findAllChatRoomByUserID(String userID) {
-        return repository.findAllByUser1OrUser2(userID, userID);
+        return chatRoomRepository.findAllByUser1OrUser2(userID, userID);
+    }
+
+    public List<ChatMessage> findAllChatMessageByChatRoomID(String chatRoomID) {
+        return chatMessageRepository.findAllByChatRoomID(chatRoomID);
     }
 }
